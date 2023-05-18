efa381d127656cf31a0447acf0e234590676f96e
Badari Narayan V <88582053+Badari103@users.noreply.github.com>
2023-05-18 10:32:13 +0530

    Create fibonacci.py

---
+++ py/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(input_number):
+    fib_sequence = [0, 1] 
+    
+    while len(fib_sequence) < input_number:
+        next_number = fib_sequence[-1] + fib_sequence[-2] 
+        fib_sequence.append(next_number)  
+    
+    return fib_sequence
+
+input_number=5
+fib_numbers = fibonacci(input_number)
+print(fib_numbers)
